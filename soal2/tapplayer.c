14dc6338056af20d10b8648a5b3a94265f26e115
Yohan Ardiansyah <yohan.ardiansyah90@gmail.com>
2020-04-02 07:13:45 +0700

    Soal 2 finished

--- soal2/tapplayer.c
+++ soal2/tapplayer.c
@@ -1,81 +1,302 @@
-#include <stdio.h>
-#include <sys/socket.h>
-#include <stdlib.h>
-#include <netinet/in.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/types.h>
-#include <sys/wait.h>
+// gcc tapplayer.c -pthread -o tapplayer
+// do sudo apt-get install libncurses5-dev
+// then "sudo apt install ncurses-doc" to see man ncurses for doc
+#include <stdio.h> 
+#include <sys/socket.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
 #include <ctype.h>
-#define PORT 8080
+#include <sys/types.h> 
+#include <sys/wait.h>
+// #include <curses.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <termios.h>
 
+#define PORT 8080 
+#define FALSE 0
+#define TRUE 1
+#define LOGIN 2
+#define REGISTER 3
+#define FIND_MATCH 4
+#define LOGOUT 5
+#define UNAUTHENTICATED 6
+#define AUTHENTICATED 7
+#define BATTLE 8
+#define WIN 9
+#define LOSE 10
 
-  
-int main(int argc, char const *argv[]) {
-    struct sockaddr_in address;
-    int sock = 0, valread, flag = -1, i, status, is_login = 0;
-    struct sockaddr_in serv_addr;
-    char input[100];
-    char buffer[1024] = {0};
-    pid_t child;
-    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-        printf("\n Socket creation error \n");
-        return -1;
-    }
-  
-    memset(&serv_addr, '0', sizeof(serv_addr));
-  
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-      
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
-        printf("\nInvalid address/ Address not supported \n");
-        return -1;
-    }
-  
-    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
-        printf("\nConnection Failed \n");
-        return -1;
-    }
-    
-    do
-    {   
-        child = fork();
-        if(child == 0) {
-            char *arr[] = {'clear', NULL};
-            execv("/bin/clear", arr);
-        }
-        while (wait(&status));
-        
-        if(flag == 0)
-            printf("Insert The Correct Command");
-        
-        flag = -1;
+int status, sock, valread;
 
-        gets(input);
+static struct termios stored_settings;
 
-        int length = strlen(input);
-        
-        for ( i = 0; i < length; i++)
-        {
-            input[i] = tolower(input[i]);
-        }
-        
-        if(strcmp(input, "login") == 0) {
-            send(sock , hello , strlen(hello) , 0 );
-            is_login = 1;
-        }else if(strcmp(input, "find") == 0 && is_login){
-
-        }else if(strcmp(input, "logout" == 0 && is_login)){
-            is_login = 0;
-        }else{
-            flag = 0;
-        }
-        // printf("Hello message sent\n");
-        // valread = read( sock , buffer, 1024);
-        // printf("%s\n",buffer );
-    } while (1);
-    
-    return 0;
+void set_keypress(void)
+{
+    struct termios new_settings;
+    tcgetattr(0,&stored_settings);
+    new_settings = stored_settings;
+    new_settings.c_lflag &= (~ICANON);
+    new_settings.c_cc[VTIME] = 0;
+    new_settings.c_cc[VMIN] = 1;
+    tcsetattr(0,TCSANOW,&new_settings);
+    return;
 }
+
+void reset_keypress(void)
+{
+    tcsetattr(0,TCSANOW,&stored_settings);
+    return;
+}
+
+int checkInput(char *input) {
+    int length = strlen(input), i;
+    
+    for ( i = 0; i < length; i++)
+    {
+        input[i] = tolower(input[i]);
+    }
+    
+    if(!strcmp(input, "login")) return LOGIN;
+    else if(!strcmp(input, "register")) return REGISTER;
+    else if(!strcmp(input, "find match") || !strcmp(input, "find")) return FIND_MATCH;
+    else if(!strcmp(input, "logout")) return LOGOUT;
+    return FALSE;
+}
+
+void clearscr ( void )
+{
+    int i;
+    for ( i = 0; i < 50; i++ )
+        putchar('\n');
+}
+
+void *loop( void *ptr )
+{
+    // int i = 0;
+    while (status == TRUE)
+    {
+        // printf("Waiting for player ... %d\n", status);
+        if(status == BATTLE || status == FALSE)break;
+        puts("Waiting for player ...");
+        sleep(1);
+        // i++;
+    } 
+    return NULL;
+}
+
+void *receiveInput( void *ptr )
+{
+    char buffer[1024];
+    memset(buffer, FALSE, sizeof(buffer));
+
+    while((valread = read(sock, buffer, 1024))) {
+        // clear();
+
+        if(!strcmp(buffer, "win")) {
+            printf("Game berakhir kamu menang\n");
+            status = WIN;
+            break;
+        }
+
+        if(!strcmp(buffer, "lose")) {
+            printf("Game berakhir kamu kalah\n");
+            status = LOSE;
+            break;
+        }
+
+        printf("%s\n", buffer);
+        // printf("%s\n", buffer);
+
+        memset(buffer, FALSE, sizeof(buffer));
+    }
+
+    status = FALSE;
+    return NULL;
+}
+
+
+
+
+int main(int argc, char const *argv[]) 
+{ 
+	int is_login = 0, flag = 0, iret; 
+	struct sockaddr_in serv_addr; 
+	char message[1024], buffer[1024] = {0}, c;
+    pthread_t thread;
+
+	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
+	{ 
+		printf("\n Socket creation error \n"); 
+		return -1; 
+	} 
+
+	serv_addr.sin_family = AF_INET; 
+	serv_addr.sin_port = htons(PORT); 
+	
+	// Convert IPv4 and IPv6 addresses from text to binary form 
+	if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) 
+	{ 
+		printf("\nInvalid address/ Address not supported \n"); 
+		return -1; 
+	} 
+
+	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
+	{ 
+		printf("\nConnection Failed \n"); 
+		return -1; 
+	}
+
+    while (1)
+    {
+        memset(buffer, FALSE, sizeof(buffer));
+        memset(message, FALSE, sizeof(message));
+
+        // if(flag != BATTLE)clearscr();
+        // else flag = FALSE;
+
+        if(flag != FALSE) {
+            if(flag == AUTHENTICATED){
+                printf("Sudah Login\n");
+            }else if(flag == LOGIN) {
+                printf("Login Terlebih Dahulu\n");
+            }
+            flag = FALSE;
+        }
+
+        if(!is_login)printf("1.Login\n2.Register\nChoices : ");
+        else printf("1.Find Match\n2.Logout\nChoices : ");
+        
+        gets(message);
+        int input = checkInput(message);
+        memset(message, FALSE, sizeof(message));
+
+        switch (input)
+        {
+        case LOGIN:
+            if(is_login){
+                flag = AUTHENTICATED;
+                printf("Input Salah\n");
+                continue;
+            }
+            
+            send(sock , "login" , strlen("login") , 0 ); 
+            
+            printf("Username : ");
+            gets(message);
+            // puts(message);
+            send(sock , message , strlen(message) , 0 );
+            memset(message, FALSE, sizeof(message));
+
+            printf("Password : ");
+            gets(message);
+            // puts(message);
+            send(sock , message , strlen(message) , 0 );
+            memset(message, FALSE, sizeof(message));
+            
+            valread = read(sock, buffer, 1024);
+            puts(buffer);
+            if(!strcmp("login success", buffer)) is_login = TRUE;
+
+            break;
+        case REGISTER:
+            if(is_login){
+                flag = AUTHENTICATED;
+                printf("Input Salah\n");
+                continue;
+            }
+
+            send(sock , "register" , strlen("register") , 0 ); 
+            
+            printf("Username : ");
+            gets(message);
+            // puts(message);
+            send(sock , message , strlen(message) , 0 );
+            memset(message, FALSE, sizeof(message));
+
+            printf("Password : ");
+            gets(message);
+            // puts(message);
+            send(sock , message , strlen(message) , 0 );
+            memset(message, FALSE, sizeof(message));
+
+            valread = read(sock, buffer, 1024);
+            puts(buffer);
+
+            break;
+        case FIND_MATCH:
+            if(!is_login){
+                flag = UNAUTHENTICATED;
+                printf("Input Salah\n");
+                continue;
+            }
+
+            status = TRUE;
+
+            send(sock , "waiting for player" , strlen("waiting for player"), 0 );
+
+            iret = pthread_create(&thread,NULL,&loop,NULL);
+
+            if(iret)
+            {
+                fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+                exit(EXIT_FAILURE);
+            }
+
+            while (valread = read(sock, buffer, 1024))
+            {
+                if(!strcmp(buffer,"found your opponent")){
+                    status = FALSE;
+                    break;
+                }
+
+                memset(buffer, FALSE, sizeof(buffer));
+            }
+
+            printf("Game dimulai silahkan tap tap secepat mungkin !!\n");
+
+            sleep(1);
+
+            status = BATTLE;
+
+            iret = pthread_create(&thread,NULL,&receiveInput,NULL);
+
+            if(iret)
+            {
+                fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+                exit(EXIT_FAILURE);
+            }
+
+            set_keypress();
+
+            while (status == BATTLE)
+            {
+                if(status != BATTLE) break;
+                c = getc(stdin);
+                if(status == WIN) break;
+                if(c == 32) {
+                    if(status != WIN) send(sock , "hit" , strlen("hit"), 0 );
+                }
+            }
+            status = FALSE;
+
+            reset_keypress();
+
+            break;
+        case LOGOUT:
+            if(!is_login){
+                flag = UNAUTHENTICATED;
+                printf("Input Salah\n");
+                continue;
+            }
+            
+            is_login = FALSE;
+            break;
+        default:
+            printf("Input Salah\n");
+            break;
+        }
+    }
+    
+	return 0; 
+} 
--- soal2/tapserver.c
+++ soal2/tapserver.c
@@ -1,89 +1,232 @@
+// gcc -pthread -o tapserver tapserver.c 
+#include <unistd.h> 
 #include <stdio.h> 
-#include <string.h> 
-#include <stdlib.h> 
-#include <errno.h> 
-#include <unistd.h>
-#include <arpa/inet.h>
-#include <sys/types.h> 
 #include <sys/socket.h> 
+#include <stdlib.h> 
 #include <netinet/in.h> 
-#include <sys/time.h> 
-#define PORT 8600
+#include <string.h> 
+#include <pthread.h>
+#include <sys/wait.h> 
+#include <sys/types.h> 
 
-int main(int argc, char const *argv[]) {
-    struct sockaddr_in address;
-    int opt = 1, addrlen = sizeof(address), max_sd, server_fd, new_socket, valread, client_socket[50] , max_clients = 50, sd, i, activity;
-    char buffer[1024] = {0};
-    char *hello = "Hello from server";
-    fd_set readfds; 
-      
-    for (i = 0; i < max_clients; i++) 
+#define PORT 8080 
+#define MAX_CLIENT 30
+#define FALSE 0
+#define TRUE 1
+#define LOGIN 2
+#define REGISTER 3
+#define FIND_MATCH 4
+#define LOGOUT 5
+#define WIN 6
+#define LOSE 7
+
+int searching;
+int health[100];
+
+void *client_thread( void *ptr );
+
+int main(int argc, char const *argv[]) 
+{ 
+	int server_fd, new_socket
+	[MAX_CLIENT]
+	, valread, i, iret[MAX_CLIENT]; 
+	struct sockaddr_in address; 
+	int opt = 1; 
+	int addrlen = sizeof(address);  
+	pthread_t threads[MAX_CLIENT];
+    FILE *fptr;
+    fptr = fopen("akun.txt","ab");
+    fclose(fptr);
+
+    searching = -1;
+    for ( i = 0; i < 100; i++)
+        health[i] = 100;
+	
+	// Creating socket file descriptor 
+	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
 	{ 
-		client_socket[i] = 0; 
+		perror("socket failed"); 
+		exit(EXIT_FAILURE); 
+	} 
+	
+	// Forcefully attaching socket to the port 8080 
+	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR , &opt, sizeof(opt))) 
+	{ 
+		perror("setsockopt"); 
+		exit(EXIT_FAILURE); 
 	} 
 
-    if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-      
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
-        perror("setsockopt");
-        exit(EXIT_FAILURE);
-    }
+	address.sin_family = AF_INET; 
+	address.sin_addr.s_addr = INADDR_ANY; 
+	address.sin_port = htons( PORT ); 
+	
+	// Forcefully attaching socket to the port 8080 
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) 
+	{ 
+		perror("bind failed"); 
+		exit(EXIT_FAILURE); 
+	}
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
-      
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
+	if (listen(server_fd, 3) < 0) 
+	{ 
+		perror("listen"); 
+		exit(EXIT_FAILURE); 
+	} 
 
-    if (listen(server_fd, 3) < 0) {
-        perror("listen");
-        exit(EXIT_FAILURE);
-    }
-
-    while (1)
-    {
-        FD_ZERO(&readfds); 
-
-        FD_SET(server_fd, &readfds);
-        max_sd = server_fd;
-
-        for ( i = 0; i < max_clients; i++)
-        {
-            sd = client_socket[i];
-
-            if(sd > 0)
-                FD_SET( sd , &readfds);
-
-            if(sd > max_sd)
-                max_sd = sd;
-        }
-
-        activity = select( max_sd + 1 , &readfds , NULL , NULL , NULL);        
-        
-        if ((activity < 0) && (errno!=EINTR)) 
+	for ( i = 0; i < MAX_CLIENT; i++)
+	{
+		if ((new_socket[i] = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) 
 		{ 
-			printf("select error"); 
-		} 
-    }
-    
-    // if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
-    //     perror("accept");
-    //     exit(EXIT_FAILURE);
-    // }
+			perror("accept"); 
+			exit(EXIT_FAILURE); 
+		}
 
-    // do
-    // {
-    //     valread = read( new_socket , buffer, 1024);
-    //     printf("%s\n",buffer );
-    //     send(new_socket , hello , strlen(hello) , 0 );
-    //     printf("Hello message sent\n");
-    // } while (1);
-    
-    return 0;
+		// printf("New Client : %d\n", new_socket[i]);
+
+		void *ress;
+		int arr[1] = {new_socket[i]};
+
+		iret[i] = pthread_create(&threads[i], NULL, client_thread, (void*) arr);
+                
+		if(iret[i]) //jika eror
+		{
+			fprintf(stderr,"Error - pthread_create() return code: %d\n",iret[i]);
+			exit(EXIT_FAILURE);
+		}
+	}
+
+	for ( i = 0; i < MAX_CLIENT; i++)
+	{
+		pthread_join(threads[i], NULL);
+	}
+	
+	return 0;
+} 
+
+void *client_thread(void *ptr) {
+	int valread;
+	char buffer[1024], message[1024], username[1024], password[1024];
+	int *new_socket = (int *)ptr;
+	int client_addr = new_socket[0];
+    FILE *fptr;
+	
+    memset(buffer, 0, sizeof(buffer));
+    memset(username, 0, sizeof(username));
+    memset(password, 0, sizeof(password));
+    memset(message, 0, sizeof(message));
+
+	while ((valread = recv(client_addr , buffer , 1024 , 0)) > 0)
+	{
+		// printf("%d %s\n", client_addr,buffer );
+        if(!strcmp("login", buffer)) {
+            fptr = fopen("akun.txt","rb");
+            int flag = FALSE;
+            if(fptr != NULL) {
+                memset(buffer, FALSE, sizeof(buffer));
+
+                valread = recv(client_addr , username , 1024 , 0);
+                valread = recv(client_addr , password , 1024 , 0);
+
+                char temp[2][1024];
+
+                while(fread(temp, sizeof(temp), 2, fptr))
+                {
+                    // puts(temp[0]);
+                    // puts(temp[1]);
+                    if(!strcmp(temp[0], username) && !strcmp(temp[1], password)) {
+                        flag = TRUE;
+                        send(client_addr , "login success" , strlen("login success") , 0 );
+                        printf("Auth success\n");
+                        break;
+                    }
+                }
+
+                fclose(fptr);
+            }
+
+            if(!flag){
+                send(client_addr , "login failed" , strlen("login failed") , 0 );
+                printf("Auth Failed\n");
+            }
+
+        }else if(!strcmp("register", buffer)) {
+            fptr = fopen("akun.txt","ab");
+            char temp[2][1024];
+            
+            memset(buffer, FALSE, sizeof(buffer));
+            
+            valread = recv(client_addr , temp[0] , 1024 , 0);
+            valread = recv(client_addr , temp[1] , 1024 , 0);
+
+            send(client_addr , "register success" , strlen("register success") , 0 );
+
+            fwrite(temp , sizeof(temp) , 2 ,  fptr );
+
+            fclose(fptr);
+
+            fptr = fopen("akun.txt","rb");
+            if(fptr != NULL) {
+                while (fread(temp, sizeof(temp), 2, fptr))
+                {
+                    printf("Username : %s Password : %s\n", temp[0], temp[1]);
+                }
+                
+                fclose(fptr);
+            }
+        }else if(!strcmp("waiting for player", buffer)) {
+            // printf("searching -> %d\n", searching);
+            int opponent_addr;
+            if(searching == -1){
+                searching = client_addr;
+
+                while (searching == client_addr)
+                {
+                }
+
+                opponent_addr = searching;
+
+            } else {
+                opponent_addr = searching;
+                searching = client_addr;
+
+                sleep(1);
+                searching = -1;
+                send(client_addr, "found your opponent", strlen("found your opponent"), 0);
+                send(opponent_addr, "found your opponent", strlen("found your opponent"), 0);
+                health[client_addr] = health[opponent_addr] = 100;
+            }
+
+            int flag = WIN;
+
+            while ((valread = recv(client_addr , buffer , 1024 , 0)) > 0)
+            {
+                health[opponent_addr] -= 10;
+
+                if(health[opponent_addr] <= 0) {
+                    send(client_addr, "win", strlen("win"), 0);
+                    send(opponent_addr, "lose", strlen("lose"), 0);
+                    break;
+                }
+
+                if(health[client_addr] <= 0) {
+                    send(client_addr, "lose", strlen("lose"), 0);
+                    send(opponent_addr, "win", strlen("win"), 0);
+                    break;
+                }
+
+                send(client_addr, "hit !!", strlen("hit !!"), 0);
+                
+                memset(message, FALSE, sizeof(message));
+                sprintf(message, "Health kamu sekarang : %d", health[opponent_addr]);
+                send(opponent_addr, message, sizeof(message), 0);
+                memset(buffer, 0, sizeof(buffer));
+            }
+            memset(buffer, 0, sizeof(buffer));
+        }
+		
+        memset(buffer, 0, sizeof(buffer));
+        memset(username, 0, sizeof(username));
+        memset(password, 0, sizeof(password));
+        memset(message, 0, sizeof(message));
+	}
 }
