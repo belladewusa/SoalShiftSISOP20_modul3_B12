0cf178d635fbe9beb5b6324b702bb416855195ef
Yohan Ardiansyah <yohan.ardiansyah90@gmail.com>
2020-04-02 20:52:05 +0700

    No 1 Testing

---
+++ soal1/soal1_pokezone.c
@@ -0,0 +1,260 @@
+// gcc -pthread -o soal1_pokezone soal1_pokezone.c
+#include <stdio.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <ctype.h>
+#include <sys/types.h> 
+#include <sys/wait.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <wait.h>
+#include <time.h>
+#include <sys/prctl.h>
+
+//Menu
+#define FALSE 0
+#define TRUE 1
+#define CARI_POKEMON 2
+#define POKEDEX 3
+#define SHOP 4
+#define BERHENTI_MENCARI 5
+#define TANGKAP 6
+#define ITEM 7
+#define KELUAR_DARI_CAPTURE 8
+#define LEPAS_POKEMON 9
+#define BERI_BERRY 10
+
+//Item
+#define LULLABY_POWDER 0
+#define POKEBALL 1
+#define BERRY 2
+
+// Shared memory index
+#define LULLABY_STORE_NUM 0
+#define POKEBALL_STORE_NUM 1
+#define BERRY_STORE_NUM 2
+#define INDEX_X_POKEMON 3
+#define INDEX_Y_POKEMON 4
+#define NEW_ESCAPE_RATE 5
+#define NEW_CAPTURE_RATE 6
+#define NEW_POKEDOLLAR 7
+#define OLD_ESCAPE_RATE 8
+#define OLD_CAPTURE_RATE 9
+#define OLD_POKEDOLLAR 10
+#define IS_LULLABY_USED 11
+
+char pokemon[3][5][100] = {
+    {
+        "Bulbasaur",
+        "Charmander",
+        "Squirtle",
+        "Rattata",
+        "Caterpie"
+    },
+    {
+        "Pikachu",
+        "Eevee",
+        "Jigglypuff",
+        "Snorlax",
+        "Dragonite"
+    },
+    {
+        "Mew",
+        "Mewtwo",
+        "Moltres",
+        "Zapdos",
+        "Articuno"
+    }
+};
+
+int encounter_rate[] = {80, 15, 5};
+int escape_rate[] = {5, 10, 20};
+int capture_rate[] = {70, 50, 30};
+int pokedollar[] = {80, 100, 200};
+int item_cost[] = {60, 5, 15};
+int *shared_array;
+int lullaby_check[100];
+
+void testingSharedArray() {
+    printf("LULLABY_STORE_NUM %d\n", shared_array[LULLABY_STORE_NUM]);
+    printf("POKEBALL_STORE_NUM %d\n", shared_array[POKEBALL_STORE_NUM]);
+    printf("BERRY_STORE_NUM %d\n", shared_array[BERRY_STORE_NUM]);
+    printf("INDEX_X_POKEMON %d\n", shared_array[INDEX_X_POKEMON]);
+    printf("INDEX_Y_POKEMON %d\n", shared_array[INDEX_Y_POKEMON]);
+    printf("NEW_ESCAPE_RATE %d\n", shared_array[NEW_ESCAPE_RATE]);
+    printf("NEW_CAPTURE_RATE %d\n", shared_array[NEW_CAPTURE_RATE]);
+    printf("NEW_POKEDOLLAR %d\n", shared_array[NEW_POKEDOLLAR]);
+    printf("OLD_ESCAPE_RATE %d\n", shared_array[OLD_ESCAPE_RATE]);
+    printf("OLD_CAPTURE_RATE %d\n", shared_array[OLD_CAPTURE_RATE]);
+    printf("OLD_POKEDOLLAR %d\n", shared_array[OLD_POKEDOLLAR]);
+    printf("IS_LULLABY_USED %d\n", shared_array[IS_LULLABY_USED]);
+    putchar('\n');
+}
+
+void * itemAdding(void * ptr) {
+    int i;
+    while (TRUE)
+    {
+        for ( i = 0; i < 10; i++)
+        {
+            if((shared_array[LULLABY_STORE_NUM] + shared_array[BERRY_STORE_NUM] + shared_array[POKEBALL_STORE_NUM]) >= 200) break;
+
+            shared_array[LULLABY_STORE_NUM] += 1;
+        }
+
+        for ( i = 0; i < 10; i++)
+        {
+            if((shared_array[LULLABY_STORE_NUM] + shared_array[BERRY_STORE_NUM] + shared_array[POKEBALL_STORE_NUM]) >= 200) break;
+
+            shared_array[POKEBALL_STORE_NUM] += 1;
+        }
+
+        for ( i = 0; i < 10; i++)
+        {
+            if((shared_array[LULLABY_STORE_NUM] + shared_array[BERRY_STORE_NUM] + shared_array[POKEBALL_STORE_NUM]) >= 200) break;
+
+            shared_array[BERRY_STORE_NUM] += 1;
+        }
+
+        // testingSharedArray();
+        sleep(10);
+    }
+    
+    return NULL;
+}
+
+void * lullabyEffect(void * ptr) {
+    int *param = (int *)ptr;
+
+    int i, count = 0, old_escape_rate;
+
+    for ( i = 0; i < 20; i++)
+    {
+        if(shared_array[OLD_CAPTURE_RATE] >= 100) break;
+        shared_array[OLD_CAPTURE_RATE] += 1;
+        count++;
+    }
+    
+    old_escape_rate = shared_array[9];
+    shared_array[OLD_ESCAPE_RATE] = 0;
+
+    sleep(10);
+
+    shared_array[OLD_CAPTURE_RATE] -= count;
+    shared_array[OLD_ESCAPE_RATE] = old_escape_rate;
+
+    lullaby_check[param[0]] = FALSE;
+
+    return NULL;
+}
+
+void * randomPokemon( void * ptr) {
+    while (TRUE)
+    {
+        int random_num = rand() % 100;
+        if(random_num < 80) shared_array[INDEX_X_POKEMON] = 0;
+        else if(random_num < 95) shared_array[INDEX_X_POKEMON] = 1;
+        else shared_array[INDEX_X_POKEMON] = 2;
+
+        shared_array[NEW_ESCAPE_RATE] = escape_rate[shared_array[INDEX_X_POKEMON]];
+        shared_array[NEW_CAPTURE_RATE] = capture_rate[shared_array[INDEX_X_POKEMON]];
+        shared_array[NEW_POKEDOLLAR] = pokedollar[shared_array[INDEX_X_POKEMON]];
+
+        // random_num = rand() % 8000;
+        random_num = rand() % 10;
+
+        // if(random_num == 77) {
+        if(random_num < 5) {
+            shared_array[NEW_ESCAPE_RATE] += 5;
+            shared_array[NEW_CAPTURE_RATE] -= 20;
+            shared_array[NEW_POKEDOLLAR] += 5000;
+        }
+
+        sleep(1);
+    }
+    return NULL;
+}
+
+int main() {
+
+    key_t key = 1234;
+    int shmid = shmget(key, sizeof(int)*20, IPC_CREAT | 0666), i;
+    shared_array = shmat(shmid, NULL, 0);
+    int irett[2];
+
+    pthread_t item_adding;
+    pthread_t lullaby_effect[100];
+    pthread_t random_pokemon;
+
+    for ( i = 0; i < 12; i++) shared_array[i] = 0;
+    shared_array[POKEBALL_STORE_NUM] = 40;
+    shared_array[LULLABY_STORE_NUM] = shared_array[BERRY_STORE_NUM] = 30;
+    shared_array[IS_LULLABY_USED] = FALSE;
+
+    irett[0] = pthread_create(&item_adding, NULL, itemAdding, NULL);
+    irett[1] = pthread_create(&random_pokemon, NULL, randomPokemon, NULL);
+
+    for ( i = 0; i < 100; i++) lullaby_check[i] = FALSE;
+    
+    pid_t checking_lullaby = fork();
+
+    if(checking_lullaby == 0) {
+        while (TRUE)
+        {
+            if(shared_array[IS_LULLABY_USED]) {
+                for ( i = 0; i < 100; i++)
+                {
+                    if(!lullaby_check[i]) {
+                        lullaby_check[i] = TRUE;
+                        int iret;
+                        int arr[1] = {i};
+
+                        iret = pthread_create(&lullaby_effect[i], NULL, lullabyEffect, (void*) arr);
+                        break;
+                    }
+                }
+                
+                shared_array[IS_LULLABY_USED] = FALSE;
+            }
+        }
+    }
+
+    char message[100];
+
+    while (TRUE)
+    {
+        puts("Ketik shutdown untuk shutdown game");
+        
+        gets(message);
+
+        int length = strlen(message);
+        
+        for ( i = 0; i < length; i++)
+        {
+            message[i] = tolower(message[i]);
+        }
+
+        if(!strcmp("shutdown", message)) break;
+        puts("Input salah");   
+    }
+
+    int status;
+    pid_t shutdown = fork();
+
+    if(shutdown == 0) {
+        char *arr[] = {"pkill","-f","soal1_traizone",NULL};
+        execv("/bin/pkill",arr);
+    }else {
+        char *arr[] = {"pkill","-f","soal1_pokezone",NULL};
+        execv("/bin/pkill",arr);
+    }
+
+    shmdt(shared_array);
+    shmctl(shmid, IPC_RMID, NULL);
+    exit(EXIT_SUCCESS);
+}
---
+++ soal1/soal1_traizone.c
@@ -0,0 +1,451 @@
+// gcc -pthread -o soal1_traizone soal1_traizone.c
+#include <stdio.h> 
+#include <arpa/inet.h> 
+#include <unistd.h> 
+#include <string.h> 
+#include <ctype.h>
+#include <sys/types.h> 
+#include <sys/wait.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <sys/ipc.h>
+#include <sys/shm.h>
+
+//Menu
+#define FALSE 0
+#define TRUE 1
+#define CARI_POKEMON 2
+#define POKEDEX 3
+#define SHOP 4
+#define BERHENTI_MENCARI 5
+#define TANGKAP 6
+#define ITEM 7
+#define KELUAR_DARI_CAPTURE 8
+#define LEPAS_POKEMON 9
+#define BERI_BERRY 10
+
+//Item
+#define LULLABY_POWDER 0
+#define POKEBALL 1
+#define BERRY 2
+
+// Shared memory index
+#define LULLABY_STORE_NUM 0
+#define POKEBALL_STORE_NUM 1
+#define BERRY_STORE_NUM 2
+#define INDEX_X_POKEMON 3
+#define INDEX_Y_POKEMON 4
+#define NEW_ESCAPE_RATE 5
+#define NEW_CAPTURE_RATE 6
+#define NEW_POKEDOLLAR 7
+#define OLD_ESCAPE_RATE 8
+#define OLD_CAPTURE_RATE 9
+#define OLD_POKEDOLLAR 10
+#define IS_LULLABY_USED 11
+
+int is_capture_mode, is_cari_pokemon, pokemon_owned[7], count_pokemon_owned, pokedollar_owned, pokedollar_pokemon_owned[7], is_first_encounter;
+int *shared_array;
+
+char pokemon[3][5][100] = {
+    {
+        "Bulbasaur",
+        "Charmander",
+        "Squirtle",
+        "Rattata",
+        "Caterpie"
+    },
+    {
+        "Pikachu",
+        "Eevee",
+        "Jigglypuff",
+        "Snorlax",
+        "Dragonite"
+    },
+    {
+        "Mew",
+        "Mewtwo",
+        "Moltres",
+        "Zapdos",
+        "Articuno"
+    }
+};
+
+char item[3][50] = {
+    "Lullaby Powder",
+    "Pokeball",
+    "Berry"
+};
+
+int affection_point[7];
+int index_pokemon[7][2];
+int item_owned[3];
+int encountered_pokemon[2];
+int item_cost[] = {60, 5, 15};
+
+void testingSharedArray() {
+    printf("LULLABY_STORE_NUM %d\n", shared_array[LULLABY_STORE_NUM]);
+    printf("POKEBALL_STORE_NUM %d\n", shared_array[POKEBALL_STORE_NUM]);
+    printf("BERRY_STORE_NUM %d\n", shared_array[BERRY_STORE_NUM]);
+    printf("INDEX_X_POKEMON %d\n", shared_array[INDEX_X_POKEMON]);
+    printf("INDEX_Y_POKEMON %d\n", shared_array[INDEX_Y_POKEMON]);
+    printf("NEW_ESCAPE_RATE %d\n", shared_array[NEW_ESCAPE_RATE]);
+    printf("NEW_CAPTURE_RATE %d\n", shared_array[NEW_CAPTURE_RATE]);
+    printf("NEW_POKEDOLLAR %d\n", shared_array[NEW_POKEDOLLAR]);
+    printf("OLD_ESCAPE_RATE %d\n", shared_array[OLD_ESCAPE_RATE]);
+    printf("OLD_CAPTURE_RATE %d\n", shared_array[OLD_CAPTURE_RATE]);
+    printf("OLD_POKEDOLLAR %d\n", shared_array[OLD_POKEDOLLAR]);
+    printf("IS_LULLABY_USED %d\n", shared_array[IS_LULLABY_USED]);
+}
+
+int checkInput(char *str) {
+    int length = strlen(str), i;
+
+    for ( i = 0; i < length; i++)
+        str[i] = tolower(str[i]);
+    
+    if(!strcmp("cari pokemon", str)) return CARI_POKEMON;
+    else if(!strcmp("berhenti mencari", str)) return BERHENTI_MENCARI;
+    else if(!strcmp("pokedex", str)) return POKEDEX;
+    else if(!strcmp("shop", str)) return SHOP;
+    else if(!strcmp("tangkap", str)) return TANGKAP;
+    else if(!strcmp("item", str)) return ITEM;
+    else if(!strcmp("keluar dari capture", str)) return KELUAR_DARI_CAPTURE;
+    else if(!strcmp("lepas pokemon", str)) return LEPAS_POKEMON;
+    else if(!strcmp("beri berry", str)) return BERI_BERRY;
+
+    return FALSE;
+}
+
+void *cariPokemon(void * ptr) {
+    int chance_got;
+
+    while (TRUE)
+    {
+        if(!is_cari_pokemon) break;
+    
+        sleep(10);
+        
+        if(!is_cari_pokemon) break;
+
+        chance_got = rand() % 100;
+
+        if(!is_cari_pokemon) break;
+
+        if(chance_got < 60) {
+            is_cari_pokemon = FALSE;
+            is_capture_mode = TRUE;
+            is_first_encounter = TRUE;
+            
+            puts("Menemukan Pokemon!!");
+            
+            encountered_pokemon[0] = shared_array[INDEX_X_POKEMON];
+            encountered_pokemon[1] = shared_array[INDEX_Y_POKEMON];
+
+            shared_array[OLD_ESCAPE_RATE] = shared_array[NEW_ESCAPE_RATE];
+            shared_array[OLD_POKEDOLLAR] = shared_array[NEW_POKEDOLLAR];
+            shared_array[OLD_CAPTURE_RATE] = shared_array[NEW_CAPTURE_RATE];
+
+            break;
+        } 
+    }
+    return NULL;
+}
+
+void * pokemonRun(void *ptr) {
+    while (TRUE)
+    {
+        sleep(20);
+        
+        if(!is_capture_mode) break;
+
+        int random_number = rand() % 100;
+
+        if(random_number < shared_array[OLD_ESCAPE_RATE]) {
+            puts("Pokemon melarikan diri");
+            
+            is_capture_mode = FALSE;
+
+            break;
+        }
+    }
+    return NULL;
+}
+
+void * pokemonThread(void * ptr) {
+    int *param = (int *)ptr;
+    int index = param[0];
+
+    while (TRUE)
+    {
+        sleep(10);
+
+        if(!is_capture_mode) affection_point[index] -= 10;
+
+        if(affection_point[index] <= 0) {
+            int random_number = rand() % 100;
+
+            if(random_number < 90) {
+                printf("Pokemon %s dengan index %d terlepas\n", pokemon[index_pokemon[index][0]][index_pokemon[index][1]], index);
+
+                pokemon_owned[index] = FALSE;
+                count_pokemon_owned--;
+                break;
+            }else affection_point[index] = 50;
+        }
+    }
+    return NULL;
+}
+
+int main() {
+    int input_num, i;
+    char input[1024];
+    is_capture_mode = is_cari_pokemon =  FALSE;
+    count_pokemon_owned = pokedollar_owned = 0;
+
+    for (i = 0; i < 7; i++)
+        pokemon_owned[i] = affection_point[i] = FALSE;
+    
+    for ( i = 0; i < 3; i++)
+        item_owned[i] = 0;
+
+    item_owned[POKEBALL] = 30;
+    
+    pthread_t thread_pokemons[7];
+    pthread_t thread_pencarian;
+    pthread_t pokemon_run;
+
+    key_t key = 1234;
+
+    int shmid = shmget(key, sizeof(int)*20, IPC_CREAT | 0666);
+    shared_array = shmat(shmid, NULL, 0);
+
+    while (TRUE)
+    {
+                        
+        printf("\nPokedollar dimiliki : %d\nPokemon dimiliki : %d\nItem dimiliki : %d\n", pokedollar_owned, count_pokemon_owned, item_owned[BERRY] + item_owned[LULLABY_POWDER] + item_owned[POKEBALL]);
+        puts("Menu :");
+        if(!is_capture_mode) {
+            if(!is_cari_pokemon)puts("1.Cari Pokemon");
+            else puts("1.Berhenti Mencari");
+            puts("2.Pokedex\n3.Shop");
+            if(is_capture_mode) puts("Pokemon Ditemukan, masukan input apa saja untuk meneruskan ke capture mode");
+            printf("Input (tuliskan menu) : ");
+
+            gets(input);
+            if(is_capture_mode)continue;
+            input_num = checkInput(input);
+
+            switch (input_num)
+            {
+            case CARI_POKEMON:
+                if(is_cari_pokemon) {
+                    puts("Sedang dalam mode mencari");
+                    continue;
+                }
+
+                is_first_encounter = FALSE;
+
+                int iret_pencarian;
+
+                is_cari_pokemon = TRUE;
+                iret_pencarian = pthread_create(&thread_pencarian, NULL, cariPokemon, NULL);
+                if(iret_pencarian) {
+                    fprintf(stderr,"Error - pthread_create() return code: %d\n",iret_pencarian);
+                    exit(EXIT_FAILURE);
+                }
+
+                break;
+            case BERHENTI_MENCARI:
+                if(!is_cari_pokemon) {
+                    puts("Tidak sedang dalam mode mencari");
+                    continue;
+                }
+
+                is_cari_pokemon = FALSE;
+
+                break;
+            case POKEDEX:
+                if(count_pokemon_owned < 1) {
+                    puts("Tidak ada pokemon yang dimiliki");
+                    continue;
+                }
+
+                for ( i = 0; i < 7; i++)
+                {
+                    if(pokemon_owned[i]) {
+                        printf("%d. Pokemon : %s | AP : %d", i, pokemon[index_pokemon[i][0]][index_pokemon[i][1]], affection_point[i]);
+                    }
+                }
+
+                puts("Menu :\n1.Lepas Pokemon\n2.Beri Berry");
+                printf("Input : ");
+
+                gets(input);
+
+                input_num = checkInput(input);
+
+                switch (input_num)
+                {
+                case LEPAS_POKEMON:
+                    puts("Masukkan index dari pokemon yang ingin dilepas (Masukkan index selain angka itu untuk keluar)");
+                    printf("Index : ");
+                    
+                    int num;
+                    scanf("%d", &num);
+
+                    if(num > 6 || num < 0 || !pokemon_owned[num])
+                        continue;
+
+                    pokedollar_owned += pokedollar_pokemon_owned[num];
+
+                    pokemon_owned[num] = FALSE;
+
+                    count_pokemon_owned--;
+
+                    break;
+                case BERI_BERRY:
+                    if(item_owned[BERRY] > 0) {
+                        puts("Anda Tidak Memiliki Berry");
+                        continue;
+                    }
+
+                    item_owned[BERRY] -= 1;
+
+                    for ( i = 0; i < 7; i++)
+                    {
+                        if(pokemon_owned[i])
+                            affection_point[i] += 10;
+                    }
+                    
+                    break;
+                default:
+                    puts("Input salah");
+                    break;
+                }
+                
+                break;
+            case SHOP:
+                for ( i = 0; i < 3; i++)
+                    printf("%s Jumlah : %d\n", item[i], item_owned[i]);
+
+                puts("Shop :");
+                for ( i = 0; i < 3; i++)
+                {
+                    printf("%d.%s Stok : %d Harga : %d/item\n", i, item[i], shared_array[i], item_cost[i]);
+                }
+                
+                printf("Masukkan index item dan jumlah (Contoh: 2 10 (Membeli 10 Berry)) : ");
+                
+                int index_item, jumlah;
+                scanf("%d %d", &index_item, &jumlah);
+
+                if(index_item < 0 || index_item > 2 || shared_array[index_item] < jumlah || (item_owned[index_item] + jumlah >= 99) || ((jumlah * item_cost[index_item]) > pokedollar_owned )){
+                    puts("Input salah atau item di store kurang atau jumlah item yang akan dimiliki terlalu banyak atau Pokedollar anda kurang");
+                    continue;
+                }
+
+                item_owned[index_item] += jumlah;
+                shared_array[index_item] -= jumlah;
+                pokedollar_owned -= (jumlah * item_cost[index_item]);
+
+                break;
+                testingSharedArray();
+            default:
+                if(!is_capture_mode) puts("Input salah");
+                break;
+            }
+        //Capture Mode
+        }else{
+            if(is_first_encounter) {
+
+                int iret = pthread_create(&pokemon_run, NULL, pokemonRun, NULL);
+
+                if(iret) {
+                    fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+                    exit(EXIT_FAILURE);
+                }
+
+                is_first_encounter = FALSE;
+            }
+
+            puts("1.Tangkap\n2.Item\n3.Keluar dari capture");
+            printf("Input (tuliskan menu) : ");
+
+            gets(input);
+            input_num = checkInput(input);
+
+            switch (input_num)
+            {
+            case TANGKAP:
+                if(item_owned[POKEBALL] < 1) {
+                    puts("Pokeball tidak cukup");
+                    continue;
+                }
+
+                item_owned[POKEBALL] -= 1;
+
+                int random_number = rand() % 100;
+                
+                if(random_number < shared_array[OLD_CAPTURE_RATE] && is_capture_mode) {
+                    is_capture_mode = FALSE;
+                    puts("Berhasil meangkap pokemon");
+                    
+                    if(count_pokemon_owned >= 7) {
+                        puts("Karena slot pokemon penuh, pokedollar ditambahkan");
+
+                        pokedollar_owned += shared_array[OLD_POKEDOLLAR];
+                        continue;
+                    }
+
+                    for ( i = 0; i < 7; i++)
+                    {
+                        if(!pokemon_owned[i]) {
+                            pokemon_owned[i] = TRUE;
+
+                            affection_point[i] = 100;
+                            
+                            index_pokemon[i][0] = encountered_pokemon[0];
+                            index_pokemon[i][1] = encountered_pokemon[1];
+
+                            pokedollar_pokemon_owned[i] = shared_array[OLD_POKEDOLLAR];
+
+                            count_pokemon_owned++;
+
+                            int arr[1] = {i};
+                        
+                            int iret = pthread_create(&thread_pokemons[i], NULL, pokemonThread, (void *) arr);
+                            
+                            if(iret) {
+                                fprintf(stderr,"Error - pthread_create() return code: %d\n",iret);
+                                exit(EXIT_FAILURE);
+                            }
+                            continue;
+                        }
+                    }
+                }else puts("Gagal menangkap");
+
+                break;
+            case ITEM:
+                if(item_owned[LULLABY_POWDER] < 1) {
+                    puts("Anda tidak memiliki lullaby powder saat ini");
+                    continue;
+                }
+
+                item_owned[LULLABY_POWDER] -= 1;
+
+                shared_array[IS_LULLABY_USED] = TRUE;
+
+                break;
+            case KELUAR_DARI_CAPTURE:
+                is_capture_mode = FALSE;
+                break;
+            default:
+                if(is_capture_mode) puts("Input salah");
+                break;
+            }
+        }
+    }
+
+    shmdt(shared_array);
+    shmctl(shmid, IPC_RMID, NULL);
+}
